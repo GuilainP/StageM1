984a8fb62bba4d0d6970b8f8449de85e1e33ab66
GuilainP <61709078+GuilainP@users.noreply.github.com>
2020-06-30 11:53:49 +0200

    -

--- include/epuck/RobotDriver.hpp
+++ include/epuck/RobotDriver.hpp
@@ -152,7 +152,7 @@ private:
     void printSensors();
     void closeConnection();
     void proxDataRawValuesToMeters();
-    void positionDataCorrectionOdemetryAndWheelsSpeed();
+    void positionDataCorrectionOdometryAndWheelsSpeed();
     void RGB565toRGB888(int width, int height, unsigned char *src, unsigned char *dst);
     double intervalAdjustment(double);
 
